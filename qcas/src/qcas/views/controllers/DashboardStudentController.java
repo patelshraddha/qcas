b68ba79586bc35bc724a60e8fe1e1778f6ba7a56
RAHUL <RAHUL@172.29.81.123>
2016-11-20 19:26:56 +1030

    "gui fxml and controllers added"

---
+++ qcas/src/qcas/views/controllers/DashboardStudentController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qcas.views.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ * FXML Controller class
+ *
+ * @author RAHUL
+ */
+public class DashboardStudentController implements Initializable {
+
+    @FXML
+    private ToggleGroup singleAnswer;
+    @FXML
+    private ToggleGroup singleAnswer1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
