c898f92a73c6242bfc90602eee6650e0358370a1
akshaythorat <athorat@andrew.cmu.edu>
2016-11-25 22:30:34 +1030

    "Professor handler class added v"

--- qcas/src/qcas/model/ProfessorHandler.java
+++ qcas/src/qcas/model/ProfessorHandler.java
@@ -17,5 +17,38 @@ import qcas.operations.user.User;
  * @author Dell
  */
 public class ProfessorHandler {
-    
+    public static void getSubject(DatabaseHandler database, User user) {
+        try {
+            ResultSet rs;
+            
+            //get the user details from the user table
+            String insertquestionsquery = "SELECT USERLOGIN.login_id as user, subject.subject_name as subject "
+                    + "FROM user_subject_relation "
+                    + "INNER JOIN qcas.USERLOGIN"
+                    + "ON user_subject_relation.user_key = USERLOGIN.user_key"
+                    + "INNER JOIN qcas.subject "
+                    + "ON user_subject_relation.subject_code = subject.subject_code"
+                    + "WHERE USERLOGIN.login_id='athorat' ";
+            PreparedStatement preparedStatement = database.getConnection().prepareStatement(insertquestionsquery);
+            preparedStatement.setString(1, user.getId());
+            // preparedStatement.execute();
+            rs = preparedStatement.executeQuery();
+            while (rs.next())
+      {
+ 
+        String subject = rs.getString("subject");
+          System.out.println(subject);
+
+       
+      }
+            
+            
+            //return null;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UserLoginTableHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return null;
+
+    }
 }
