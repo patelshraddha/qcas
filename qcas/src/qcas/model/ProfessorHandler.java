d6c9a9e4407b47c164170c6fa236a16644700a18
akshaythorat <athorat@andrew.cmu.edu>
2016-11-25 21:10:36 +1030

    "Professor handler class added"

---
+++ qcas/src/qcas/model/ProfessorHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qcas.model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import qcas.operations.user.User;
+/**
+ *
+ * @author Dell
+ */
+public class ProfessorHandler {
+    
+}
